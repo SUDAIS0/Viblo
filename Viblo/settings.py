3e59c6b6d745f8e8a511d2b778fea70e0273cbca
sudais0 <sudais124578@gmail.com>
2025-05-03 02:04:21 +0500

    For Deployment

--- Viblo/settings.py
+++ Viblo/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files
@@ -124,3 +124,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = ['https://viblo.up.railway.app']
+
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / 'static']
